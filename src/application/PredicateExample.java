40d446d94b30007c0792e9d6e2afefca1f517891
felipeperaltacheung <f33___@hotmail.com>
2022-05-11 16:29:22 -0300

    Expressão lambda declarada

--- src/application/PredicateExample.java
+++ src/application/PredicateExample.java
@@ -6,6 +6,7 @@ import util.ProductPredicate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 public class PredicateExample {
 
@@ -18,9 +19,13 @@ public class PredicateExample {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
         list.add(new Product("mouse", 50.00));
-        list.add(new Product("monitor", 1.00));
+        list.add(new Product("monistor", 1.00));
 
-        list.removeIf(Product::nonStaticProductPredicate);
+        double min = 800.00;
+
+        Predicate<Product> pred = p -> p.getPrice() >= min;
+
+        list.removeIf(pred);
 
         for (Product p:list) {
             System.out.println(p);
