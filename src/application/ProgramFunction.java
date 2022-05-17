23091aa76698fc3e709a858c18b5e9b09ef907d7
felipeperaltacheung <f33___@hotmail.com>
2022-05-17 15:10:58 -0300

    Expressao lambda inline

--- src/application/ProgramFunction.java
+++ src/application/ProgramFunction.java
@@ -16,14 +16,12 @@ public class ProgramFunction {
         List<Product> list = new ArrayList<>();
 
         list.add(new Product("arsrasrasrsarsars", 700.00));
-        list.add(new Product("Notebook", 10.00));
-        list.add(new Product("Tablet", 100.00));
-        list.add(new Product("mouse", 1.00));
-        list.add(new Product("ronistor", 1000.00));
+        list.add(new Product("asasasas", 10.00));
+        list.add(new Product("tftftftft", 100.00));
+        list.add(new Product("hdhdhdhd", 1.00));
+        list.add(new Product("televisor", 1000.00));
 
-        Function<Product, String> func = p -> p.getName().toUpperCase();
-
-        List<String> names = list.stream().map(func).collect(Collectors.toList());
+        List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 
         names.forEach(System.out::println);
 
