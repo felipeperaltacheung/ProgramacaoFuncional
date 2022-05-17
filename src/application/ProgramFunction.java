4715acd7bbad28be95e2eb27a5b09a3070962153
felipeperaltacheung <f33___@hotmail.com>
2022-05-17 15:00:33 -0300

    Implementacao da interface

--- src/application/ProgramFunction.java
+++ src/application/ProgramFunction.java
@@ -1,18 +1,17 @@
 package application;
 
 import entities.Product;
-import util.PriceUpdate;
+import util.UpperCaseName;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
-public class ProgramConsumer {
+public class ProgramFunction {
 
     public static void main(String[] args) {
 
-        Locale.setDefault(Locale.US);
         List<Product> list = new ArrayList<>();
 
         list.add(new Product("TV", 700.00));
@@ -21,9 +20,9 @@ public class ProgramConsumer {
         list.add(new Product("mouse", 1.00));
         list.add(new Product("monistor", 1000.00));
 
-        list.forEach(new PriceUpdate()); // Com implementacao da interface
+        List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
 
-        list.forEach(System.out::println);
+        names.forEach(System.out::println);
 
     }
 
